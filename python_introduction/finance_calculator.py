91f7a5ac1292f2f4a4fa70e456ceb34a2ba9100d
Abdullah0x7 <belloabdullah76@gmail.com>
2025-11-07 14:31:00 -0800

    changed data type

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,9 @@
 inc = input("Enter your monthly income: ")
-income = int(inc)
+income = float(inc)
 tme = input("Enter your total monthly expenses: ")
-tot_monthly_exp = int(tme)
-monthly_savings = income - tot_monthly_exp
+tot_monthly_exp = float(tme)
+msavs = income - tot_monthly_exp
+monthly_savings = float(msavs)
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 print(f"Your monthly savings are {monthly_savings}")
 print(f"Projected savings after one year, with interest, is: {projected_savings}")
