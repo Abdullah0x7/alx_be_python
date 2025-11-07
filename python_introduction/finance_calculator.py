ce02eb27dd9ed5bcdbba061a14681d0962acaa14
Abdullah0x7 <belloabdullah76@gmail.com>
2025-11-07 14:23:17 -0800

    added monthly savings output

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,4 +4,5 @@ tme = input("Enter your total monthly expenses: ")
 tot_monthly_exp = int(tme)
 monthly_savings = income - tot_monthly_exp
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"Your monthly savings are {monthly_savings}")
 print(f"Projected savings after one year, with interest, is: {projected_savings}")
