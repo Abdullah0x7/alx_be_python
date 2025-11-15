59c90da0b8caff3586f0cdd6c3ec5563abf6927f
Abdullah0x7 <belloabdullah76@gmail.com>
2025-11-15 15:11:17 -0800

    checker requirements

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,4 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1,11):
-    product = i * number
-    print(f"\n{number} * {i} = {product}", end="\t")
-print()
+    print(f"{number} * {i} = {number * i}")
